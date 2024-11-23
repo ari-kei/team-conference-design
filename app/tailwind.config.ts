fa584f0f4972f8c6603c0c42df75472123b1d5b1
arikei <a.ri.777baseball@gmail.com>
2024-11-23 19:48:58 +0900

    カラーパレットを登録

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -7,6 +7,17 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      primary: "#3758F9",
+      secondary: "#13C296",
+      danger: "#FF4B4B",
+      stroke: "#DFE4EA",
+      icon: "#70757A",
+      "text-primary": "#637381",
+      "text-secondary": "#8899A8",
+      "text-forcus": "#6CBAFF",
+      "text-link": "#6CBAFF",
+    },
     extend: {
       colors: {
         background: "var(--background)",
