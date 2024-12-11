0a874d4b0981509ab68f76a69e8c3f8445223988
arikei <a.ri.777baseball@gmail.com>
2024-12-11 18:59:18 +0900

    presentarで受け取った緯度経度をMapコンポーネントに横流し

--- app/src/app/map/Map/presentation.tsx
+++ app/src/app/map/Map/presentation.tsx
@@ -3,10 +3,14 @@
 import Map from "@/app/_components/map/map";
 import "leaflet/dist/leaflet.css";
 
-export default function MapPresentation() {
+type MapPresentationProps = {
+  geocodes: { lat: number; lng: number }[];
+};
+
+export default function MapPresentation({ geocodes }: MapPresentationProps) {
   return (
     <div className="relative z-0">
-      <Map />
+      <Map geocodes={geocodes} />
     </div>
   );
 }
