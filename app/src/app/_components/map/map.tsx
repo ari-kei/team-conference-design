e868b0465cc4ac633a175e63e6d7b4b5bf17c98e
arikei <a.ri.777baseball@gmail.com>
2024-12-11 19:00:59 +0900

    Mapの見た目をGoogleMapライクに変更

--- app/src/app/_components/map/map.tsx
+++ app/src/app/_components/map/map.tsx
@@ -22,7 +22,7 @@ export default function Map() {
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        url="https://mt1.google.com/vt/lyrs=r&x={x}&y={y}&z={z}"
       />
       <Marker position={[51.505, -0.09]}>
         <Popup>
