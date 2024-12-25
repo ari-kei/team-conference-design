3b7bf9d696bb23a6a9821bf2346330076cf48cd6
arikei <a.ri.777baseball@gmail.com>
2024-12-25 19:21:27 +0900

    緯度経度の取得処理を地図のcontainerからヘッドラインのcontainerへ移管

--- app/src/app/map/MeetingHeadlines/container.tsx
+++ app/src/app/map/MeetingHeadlines/container.tsx
@@ -1,8 +1,20 @@
+import { GeoCode } from "@/app/domain/map";
 import MeetingHeadlinesPresentation from "./presentation";
 import { fetchList } from "@/app/_lib/fetcher/meeting";
+import { fetchAddress } from "@/app/_lib/fetcher/map";
 
 export async function MeetingHeadlinesContainer() {
   const meetings = await fetchList();
 
-  return <MeetingHeadlinesPresentation meetings={meetings} />;
+  const geocodes: GeoCode[] = [];
+  for (const meeting of meetings) {
+    // 住所1つごとにAPIを呼ばないといけない。Dataloader使えないか検討(API側が対応しているかから)
+    const longLati = await fetchAddress(meeting.address);
+    const geocode = Object.assign(longLati, { meetingId: meeting.id });
+    geocodes.push(geocode);
+  }
+
+  return (
+    <MeetingHeadlinesPresentation meetings={meetings} geocodes={geocodes} />
+  );
 }
