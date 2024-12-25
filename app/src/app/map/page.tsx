2bf0ecba9cd9435dc5685bcd4dfba523c7d7a0a9
arikei <a.ri.777baseball@gmail.com>
2024-12-25 19:20:23 +0900

    地図と会のヘッドラインのクライアント側のコードが同一ファイルにないと、マーカクリック時の詳細表示が実現しにくいため統合

--- app/src/app/map/page.tsx
+++ app/src/app/map/page.tsx
@@ -1,11 +1,8 @@
 import React from "react";
 import MeetingHeadlinesContainer from "./MeetingHeadlines";
-import MapContainer from "./Map/container";
-
 export default function MapPage() {
   return (
     <div>
-      <MapContainer />
       <MeetingHeadlinesContainer />
     </div>
   );
